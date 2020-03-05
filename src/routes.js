9a76c4997a5a3e1debea69c5c256779b0fcc6a1a
Cassia <cassia@ssinformatica.net>
2020-03-05 16:05:43 -0300

    Routes

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Main from './Pages/Main';
-import Repository from './Pages/Repository';
+import Main from './pages/Main';
+import Repository from './pages/Repository';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Main} />
+        <Route path="/" exact component={Main} />
         <Route path="/repository" component={Repository} />
       </Switch>
     </BrowserRouter>
