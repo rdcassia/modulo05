02dc70494e1fa2bdb784e276bd122026d8c1b5f5
rdcassia.alves <rdcassia.alves@gmail.com>
2020-03-06 06:23:00 -0300

    Style Components

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// import { Container } from './styles';
+import { FaGitAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return <h1>Main</h1>;
+export default class Main extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>
+          <FaGitAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={() => {}}>
+          <input type="text" placeholder="adcionar repositório" />
+
+          <SubmitButton disable>
+            <FaPlus color="FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
